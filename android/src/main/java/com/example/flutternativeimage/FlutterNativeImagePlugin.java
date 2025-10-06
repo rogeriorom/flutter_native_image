587743d2f9b0b62b6e5484ebaaccb6b118c27d00
Rogério Massa <rogerio.massa@uds.com.br>
2025-10-06 10:41:45 -0300

    updating

--- android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
+++ android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
@@ -2,43 +2,39 @@ package com.example.flutternativeimage;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
 
 /**
  * FlutterNativeImagePlugin
+ * Plugin atualizado para Flutter embedding v2
  */
 public class FlutterNativeImagePlugin implements FlutterPlugin {
-  private static final String CHANNEL_NAME = "flutter_native_image";
-  private MethodChannel channel;
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(PluginRegistry.Registrar registrar) {
-    final FlutterNativeImagePlugin plugin = new FlutterNativeImagePlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
-  }
+    private static final String CHANNEL_NAME = "flutter_native_image";
+    private MethodChannel channel;
 
-  @Override
-  public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
-    setupChannel(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
-  }
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        setupChannel(binding.getBinaryMessenger(), binding.getApplicationContext());
+    }
 
-  @Override
-  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
-    teardownChannel();
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        teardownChannel();
+    }
 
-  private void setupChannel(BinaryMessenger messenger, Context context) {
-    channel = new MethodChannel(messenger, CHANNEL_NAME);
-    MethodCallHandlerImpl handler = new MethodCallHandlerImpl(context);
-    channel.setMethodCallHandler(handler);
-  }
+    private void setupChannel(BinaryMessenger messenger, Context context) {
+        channel = new MethodChannel(messenger, CHANNEL_NAME);
+        MethodCallHandlerImpl handler = new MethodCallHandlerImpl(context);
+        channel.setMethodCallHandler(handler);
+    }
 
-  private void teardownChannel() {
-    channel.setMethodCallHandler(null);
-    channel = null;
-  }
+    private void teardownChannel() {
+        if (channel != null) {
+            channel.setMethodCallHandler(null);
+            channel = null;
+        }
+    }
 }
--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,11 +1,16 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <Flutter/Flutter.h>
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+  
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
